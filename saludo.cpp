4ece24abaebbcb5d5449231ffc5dcc6630deb176
Lautaro Solis <solislautaroaugusto@gmail.com>
2025-01-17 18:03:50 -0300

    agregando el archivo de saludo

---
+++ saludo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+ string nombre = "Lautaro";
+ string apellido = "Solis"; 
+
+ string saludo = "Hola "+ nombre + " " + apellido+". ¿Cómo estás?"; 
+
+ cout<<saludo;  
+ 
+
+    return 0;
+}
