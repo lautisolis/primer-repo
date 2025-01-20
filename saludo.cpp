defe4ee4fd7c2931c28c8bda145739cc6e0ceabe
Lauti Solis <solislautaroaugusto@gmail.com>
2025-01-20 17:53:38 -0300

    cambiando apellido

--- saludo.cpp
+++ saludo.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 
  string nombre = "Lautaro";
- string apellido = "Solis"; 
+ string apellido = "Deica"; 
 
  string saludo = "Hola "+ nombre + " " + apellido+". ¿Cómo estás?"; 
 
